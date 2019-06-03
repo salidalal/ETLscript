55896db96503f72e260510f5b5d7558ecaeecb57
sdalal <salida@ac.sce.ac.il>
2019-06-03 16:25:56 +0300

    weeklyscript - the script prints basic info,wrong calc in the avg

--- venv/weeklyScript.py
+++ venv/weeklyScript.py
@@ -15,6 +15,7 @@ logsDirc = "C:/Users/sdalal/OneDrive - ECI Telecom LTD/PycharmProjects/untitled/
 envDirc = "//netstore2/sdh_tst_pub/TamarS/LIreport"
 
 
+# getting enviroments into a dictionary where the obi name is the key and empty list is the value
 for filename in os.listdir(envDirc):
     if filename.endswith("txt"):
         with open (os.path.join(envDirc, filename)) as f:
@@ -27,13 +28,15 @@ for filename in os.listdir(envDirc):
 
 cur_month = datetime.now()
 cur_month = cur_month.strftime("%b")
+
+#looking for the daily report in the current month and put the data in the dictionary list for each enviro.
 for filename in os.listdir(logsDirc):
     if filename.endswith(cur_month+".xls"):
         addToList(os.path.join(logsDirc, filename), environmentsLogs)
 
 
-
-
+"""
+#adding sheet for each env.
 for envName in environmentsLogs.keys():
     outputLog = xlwt.Workbook(encoding="utf-8")
     try:
@@ -43,6 +46,12 @@ for envName in environmentsLogs.keys():
     except:
         outputLog = xlwt.Workbook()
 
+"""
+outputLog = xlwt.Workbook(encoding="utf-8")
 
-    printToFile(outputLog,envName,environmentsLogs)
-    outputLog.save("C:/Users/sdalal/OneDrive - ECI Telecom LTD/PycharmProjects/untitled/venv/logs/" + envName)
+#calling print to file for each sheet ( each shhet for each env)
+for env in environmentsLogs:
+    print(env)
+    printToFile(outputLog, env, environmentsLogs, cur_month)
+
+outputLog.save("C:/Users/sdalal/OneDrive - ECI Telecom LTD/PycharmProjects/untitled/venv/logs/" + cur_month+".xls")
--- venv/weeklyScriptsDefines.py
+++ venv/weeklyScriptsDefines.py
@@ -1,7 +1,7 @@
 
 # !/usr/bin/env python3
 # coding: utf-8
-from datetime import datetime
+from datetime import *
 import xlrd
 import xlwt
 style = xlwt.easyxf('align: horiz center')
@@ -23,21 +23,53 @@ def addToList(file,envList):
         row = env.row_values(1,0,14)
         envList[row[1]]+=[row]
 
-def printToFile(outputLog,envName,envLogs):
+
+def calcAvg(envList):
+    totalTime= timedelta()
+    counterSucc = 0
+    counterFailed = 0
+
+    for day in envList:
+        if("SUCCESS" in day[2]):
+            counterSucc+=1
+            (h,s,m) = day[4].split(':')
+            totalTime += timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+        else:
+            counterFailed+=1
+    avg = 0
+    if counterSucc>0:
+        print(totalTime)
+        avg =  totalTime.total_seconds()/(counterSucc)
+        print(timedelta(seconds=avg))
+    return (timedelta(seconds=avg), counterSucc, counterFailed)
+
+def printToFile(outputLog,envName,envLogs,month):
 
     sheet = outputLog
-    sheet = outputLog.add_sheet(month+" analysis")
+    sheet = outputLog.add_sheet(envName+" analysis")
     row = 0
     col = 0
 
     sheet.write(row, col, "OBI Host Name: ", titleStyle)
-    sheet.write(row + 1, col, self.getOBIhost, style)
+    sheet.write(row + 1, col, envLogs[envName][0][0], style)
     sheet.write(row, col + 1, "DWH Host Name: ", titleStyle)
-    sheet.write(row + 1, col + 1, self.getDWHhost, style)
-    sheet.write(row, col + 2, "Last ETL status: ", titleStyle)
+    sheet.write(row + 1, col + 1, envLogs[envName][0][1], style)
+
+    #returns tuple - 0: avg time, 1: num of successed etls , 2: num of failed etls
+    calc = calcAvg(envLogs[envName])
+    sheet.write(row, col + 2, "ETL avg. time: (passed)", titleStyle)
+    sheet.write(row + 1, col + 2,str(calc[0]) , style)
+
+    sheet.write(row, col + 3, "Number of passed ETLs", titleStyle)
+    sheet.write(row + 1, col + 3, calc[1], style)
+
+    sheet.write(row, col + 4, "Number of failed ETLs", titleStyle)
+    sheet.write(row + 1, col + 4, calc[2], style)
 
 
-
+    #settings cols size
+    for i in range(6):
+        sheet.col(i).width = (15) * 367
 
 
 
